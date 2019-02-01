ecfd6b05e1098f2a6b191206bd638f58aefef8ba
smharjan <smharjan@memphis.edu>
2019-02-01 22:13:52 +0000

    Fixed bug in routes page for about action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'page#home'
   get 'home', to: 'page#home', as: 'home'
-  get 'about', to: 'page#about' as: 'about'
+  get 'about', to: 'page#about', as: 'about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
